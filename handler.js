7e66420321011b502c8ef2738f79b29ab8d52eb3
Diego Naranjo <diego.naranjo@globant.com>
2022-04-11 09:15:54 -0500

    fix: github actions

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -16,3 +16,23 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+const goodbye = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+}
+
+
+module.exports = {
+  hello,
+  goodbye,
+}
